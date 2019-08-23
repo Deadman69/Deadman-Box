98f42c8a38917bf21be64a711add61b7f8fd5dab
Deadman <enzo.richard2014@gmail.com>
2019-08-24 00:09:55 +0200

    Update init.lua

--- deadman_box/lua/entities/deadman_box_box/init.lua
+++ deadman_box/lua/entities/deadman_box_box/init.lua
@@ -1,44 +1,18 @@
-AddCSLuaFile("cl_init.lua") -- DONT TOUCH IT
-AddCSLuaFile("shared.lua") -- DONT TOUCH IT
-include("shared.lua") -- DONT TOUCH IT
+AddCSLuaFile("shared.lua")
+AddCSLuaFile( "deadman_box_config.lua" ) 
+include("shared.lua") 
+include( "deadman_box_config.lua" )
 
-function ENT:Initialize() -- DONT TOUCH IT
+local entModel = DBOX.BoxModel
+local maxItems = DBOX.MaxItemsInside
+local timeBeforeEnter = DBOX.TimeBeforeEnter
+local vectorToSpawn = DBOX.VectorToSpawn
+local blacklistItems = DBOX.BlacklistItems
+local blacklistModels = DBOX.BlacklistModels
 
+function ENT:Initialize() 
 
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
--- START OF USER CONFIG DONT TOUCH UPPER THESES LINES
-
-	self:SetModel("models/props_junk/wood_crate001a.mdl") -- Model of the box
-    self.maxitems = 3 -- Max items inside this entity
-    self.timebeforereenter = 3 -- Seconds before an item wich has been pulled out can be put in the box
-    self.vectortospawn = Vector( 0, 0, 30 ) -- The position the items will spawn (relative to the box)
-    self.blacklistitems = { -- Items that will never be in the box
-    	"deadman_box_box", -- Strongly recommended to let it
-    	"deadman_box_helper", -- Strongly recommended to let it
-
-    	"npc_crow",
-    	"npc_seagull",
-    	"npc_pigeon",
-    }
-
-    self.blacklistmodels = { -- Models that will never be in the box
-    	"models/genius/incredible/wouhaou/model.mdl",
-    	"models/props/blabla.mdl"
-    }
-
-
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
--- END OF USER CONFIG DONT TOUCH BELOW THESES LINES
+	self:SetModel(entModel)
 
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
@@ -47,42 +21,48 @@ function ENT:Initialize() -- DONT TOUCH IT
     local phys = self:GetPhysicsObject()
     phys:Wake()
 
+end
 
+function ENT:SetupDataTables()
 
-    self.itemsinside = 0 -- Don't touch it
-    self.timeuse = 0 -- Don't touch it
-    self.used = false -- Don't touch it
-    self.stored = nil -- Don't touch it
-    self.model = nil -- Don't touch it
+	self:NetworkVar( "Int", 0, "ItemsInside" )
+	self:NetworkVar( "Int", 1, "TimeUse" )
+	self:NetworkVar( "Bool", 0, "Used" )
+	self:NetworkVar( "String", 0, "Stored" )
+	self:NetworkVar( "String", 1, "ModelStored" )
 
+	self:SetItemsInside(0) -- Default Value
+	self:SetTimeUse(0) -- Default Value
+	self:SetUsed(false) -- Default Value
+	self:SetStored("model") -- Default Value
+	self:SetModelStored("model") -- Default Value
 end
 
 function ENT:StartTouch( ent )
+	if ent:IsPlayer() then return end 
 
-	if table.HasValue( self.blacklistitems, ent:GetClass() ) then return end -- Si il est dans la blacklist items on annule tout
-	if table.HasValue( self.blacklistmodels, ent:GetModel() ) then return end -- Si il est dans la blacklist models on annule tout
-	if self.timeuse + self.timebeforereenter >= CurTime() then return end -- Si le temps est inférieur au temps nécéssaire on annule tout
+	if blacklistItems[ent:GetClass()] then return end -- Si il est dans la blacklist items on annule tout
+	if blacklistModels[ent:GetModel()] then return end -- Si il est dans la blacklist models on annule tout
+	if self:GetTimeUse() + timeBeforeEnter >= CurTime() then return end -- Si le temps est inférieur au temps nécéssaire on annule tout
 
-	if not self.used then -- Si il n'est pas utilisé
-		if self.itemsinside <= self.maxitems then -- Si il n'est pas pleins
+	if not self:GetUsed() then -- Si il n'est pas utilisé
+		if self:GetItemsInside() < maxItems then -- Si il n'est pas pleins
             if ent:GetClass() == "spawned_weapon" then -- Si c'est une arme
-            	self.stored = ent:GetWeaponClass()
-				self.used = true
-				self.itemsinside = self.itemsinside + 1
-				self.model = "models/weapons/w_rif_ak47.mdl"
+            	self:SetStored(ent:GetWeaponClass())
+				self:SetModelStored("models/weapons/w_rif_ak47.mdl")
 				ent:Remove()
 			else -- Si c'est autre chose (un props, une entitée...)
-				self.stored = ent:GetClass()
-				self.used = true
-				self.itemsinside = self.itemsinside + 1
-				self.model = ent:GetModel()
-				ent:Remove()
+				self:SetStored(ent:GetClass())
+				self:SetModelStored(ent:GetModel())
             end
+            self:SetUsed(true)
+			self:SetItemsInside( self:GetItemsInside() + 1)
+			ent:Remove()
 		end
 	else -- Si la boite est déjà utilisée
-		if ent:GetClass() ~= self.stored or ent:GetModel() ~= self.model then return end -- Si le classe/model de l'ent est différent du stored, on annule tout
-		if self.itemsinside <= self.maxitems then
-			self.itemsinside = self.itemsinside + 1
+		if ent:GetClass() ~= self:GetStored() or ent:GetModel() ~= self:GetModelStored() then return end -- Si le classe/model de l'ent est différent du stored, on annule tout
+		if self:GetItemsInside() < maxItems then
+			self:SetItemsInside( self:GetItemsInside() + 1)
 			ent:Remove()
 		end
 	end
@@ -93,22 +73,28 @@ end
 
 function ENT:Use(activator, caller, usetype, value)
 
-	if not activator:IsPlayer() or not activator:Alive() or not activator:IsValid() then return end
+	if not activator:IsValid() or not activator:IsPlayer() or not activator:Alive() then return end
 
-	if self.used and self.itemsinside > 0 then
+	if self:GetUsed() and self:GetItemsInside() > 0 then
 
-		local ent = self.stored
+		local ent = self:GetStored()
 		local spawnit = ents.Create( ent )
-			if ( !IsValid( spawnit ) ) then return end
-			spawnit:SetModel( self.model )
-			spawnit:SetPos( self:GetPos() + self.vectortospawn )
-			spawnit:SetOwner(activator)
-			spawnit:Spawn()
-		self.timeuse = CurTime()
-		self.itemsinside = self.itemsinside - 1
+		if ( !IsValid( spawnit ) ) then return end
+		spawnit:SetModel( self:GetModelStored() )
+		spawnit:SetPos( self:GetPos() + vectorToSpawn )
+		spawnit:SetOwner(caller)
+		spawnit:CPPISetOwner(caller)
+		spawnit:PhysicsInit(SOLID_VPHYSICS)
+		spawnit:SetMoveType(MOVETYPE_VPHYSICS)
+		spawnit:SetSolid(SOLID_VPHYSICS)
+		spawnit:SetUseType(SIMPLE_USE)
+		spawnit:Spawn()
+		spawnit:Activate()
+		self:SetTimeUse(CurTime())
+		self:SetItemsInside( self:GetItemsInside() - 1)
 
-		if self.itemsinside == 0 then
-			self.used = false
+		if self:GetItemsInside() == 0 then
+			self:SetUsed(false)
 		end
 
     end
